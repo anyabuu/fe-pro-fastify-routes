9bbd8860b71defdb87c25d4a387303a6f6277a21
Anya Bu <annabukhtiiarova@gmail.com>
2022-05-17 23:34:35 +0300

    removed to index.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 import fastify from './index';
-import {users} from "./users";
 
 (async () => {
   try {
@@ -8,56 +7,3 @@ import {users} from "./users";
     console.log(err);
   }
 })();
-
-fastify.post('/uppercase', (request, reply) => {
-
-  if(request.body.toLowerCase().indexOf('fuck') === -1) {
-    return reply.status(200).send(request.body.toUpperCase())
-  } else {
-    return reply.status(403).send('unresolved')
-  }
-
-});
-
-fastify.post('/lowercase', (request, reply) => {
-
-  if(request.body.toLowerCase().indexOf('fuck') === -1) {
-    return reply.status(200).send(request.body.toLowerCase())
-  } else {
-    return reply.status(403).send('unresolved')
-  }
-
-});
-
-fastify.get('/user/:id', (request, reply) => {
-
-  if(users[request.params.id] === undefined){
-    return reply.status(400).send('User not exist')
-  } else {
-    return reply.send(users[request.params.id])
-  }
-
-});
-
-
-fastify.get('/users', (request, reply) => {
-
-  let arr = Object.values(users)
-
-  if(!request.query.filter || !request.query.value) {
-
-    return reply.send(arr)
-
-  } else {
-
-    let filteredArr = arr.filter(function (item, i, arr) {
-
-      return arr[i][request.query.filter].toString() === request.query.value
-    })
-
-    return reply.send(filteredArr)
-  }
-
-});
-
-
